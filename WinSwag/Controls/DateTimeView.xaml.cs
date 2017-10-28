1d7820a16a5b04f09b8a3018649ae82b3cd36cb7
Sven Vinkemeier <svenvinkemeier@live.de>
2017-10-28 23:56:23 +0200

    Improved date/time picking experience

---
+++ WinSwag/Controls/DateTimeView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WinSwag.Controls
+{
+    public sealed partial class DateTimeView : UserControl
+    {
+        public static readonly DependencyProperty DateTimeProperty =
+            DependencyProperty.Register(nameof(DateTime), typeof(DateTimeOffset?), typeof(DateTimeView), new PropertyMetadata(null, OnDateTimeChanged));
+
+        private static void OnDateTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+            ((DateTimeView)d).OnDateTimeChanged((DateTimeOffset?)e.NewValue);
+
+        public DateTimeOffset? DateTime
+        {
+            get { return (DateTimeOffset?)GetValue(DateTimeProperty); }
+            set { SetValue(DateTimeProperty, value); }
+        }
+
+        public DateTimeView() => InitializeComponent();
+
+        private void OnDateTimeChanged(DateTimeOffset? newValue)
+        {
+            Time.Time = newValue?.TimeOfDay ?? TimeSpan.Zero;
+            Date.SelectedDates.Clear();
+            if (newValue.HasValue)
+                Date.SelectedDates.Add(newValue.Value);
+        }
+
+        private void OnDateChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            if (Date.SelectedDates.Any())
+                DateTime = Date.SelectedDates[0].Date + Time.Time;
+        }
+
+        private void OnTimeChanged(object sender, TimePickerValueChangedEventArgs e)
+        {
+            if (DateTime.HasValue)
+                DateTime = DateTime.Value.Date + e.NewTime;
+        }
+    }
+}
---
+++ WinSwag/ViewModels/StringToDateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace WinSwag.ViewModels
+{
+    public class StringToDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (DateTimeOffset.TryParse(value?.ToString() ?? "", out var result))
+                return result;
+            return default(DateTimeOffset?);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTimeOffset dateTimeOffset)
+                return dateTimeOffset.ToString("s");
+            if (value is DateTime dateTime)
+                return dateTime.ToString("s");
+            return value?.ToString();
+        }
+    }
+}
