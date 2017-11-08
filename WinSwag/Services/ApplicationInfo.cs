a7b3e4e897d88ca489b0740609be03b330530ca2
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-08 15:37:57 +0100

    Added "About WinSwag"-button to dashboard

--- WinSwag/Services/ApplicationInfo.cs
+++ WinSwag/Services/ApplicationInfo.cs
@@ -1,9 +1,13 @@
-﻿using Windows.ApplicationModel;
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.System;
 
 namespace WinSwag.Services
 {
     public class ApplicationInfo
     {
+        private static readonly Uri GitHubUrl = new Uri("https://github.com/SvenEV/WinSwag");
+
         public string Version
         {
             get
@@ -11,6 +15,11 @@ namespace WinSwag.Services
                 var v = Package.Current.Id.Version;
                 return $"v{v.Major}.{v.Minor}.{v.Build}.{v.Revision}";
             }
+        }
+
+        public async void OpenGitHubWebsite()
+        {
+            await Launcher.LaunchUriAsync(GitHubUrl);
         }
     }
 }
